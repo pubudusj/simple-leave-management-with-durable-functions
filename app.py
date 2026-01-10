73944645f02a9b6a99ce5ca611ac5a7237196c6c
Pubudu Jayawardana <pubudu.jayawardana@postnl.nl>
2026-01-10 12:01:27 +0100

    Init

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from simple_leave_management_with_durable_functions.simple_leave_management_with_durable_functions_stack import (  # pylint: disable=line-too-long
+    SimpleLeaveManagementWithDurableFunctionsStack,
+)
+
+
+app = cdk.App()
+SimpleLeaveManagementWithDurableFunctionsStack(
+    app,
+    "SimpleLeaveManagementWithDurableFunctionsStack",
+)
+
+app.synth()
